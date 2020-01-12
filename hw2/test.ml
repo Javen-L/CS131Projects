ed25bcdc8eb98de5cea2528f32ca9558e57153e8
Alexander Zilong Ma <alexandm@lnxsrv09.seas.ucla.edu>
2020-01-12 12:36:08 -0800

    Tested function on symbol type.

--- hw2/test.ml
+++ hw2/test.ml
@@ -1,5 +1,12 @@
+type awksub_nonterminals =
+  | Expr | Lvalue | Incrop | Binop | Num
+type ('nonterminal, 'terminal) symbol =
+        | N of 'nonterminal
+        | T of 'terminal
 let x = [1;2;3];;
 let y = [3;2;1];;
+let z = [N Expr; N Binop];;
+let w = [[T "fine"; N Incrop]; [N Expr]];;
 let func keys values term =
 	let rec findindex key lst index = match lst with
 		| [] -> -1
@@ -16,3 +23,6 @@ let test2 = func x y;;
 let output = test2 1;;
 let output = test2 2;;
 let output = test2 3;;
+let test3 = func z w;;
+let output = test3 (N Expr);;
+let output = test3 (N Binop);;
