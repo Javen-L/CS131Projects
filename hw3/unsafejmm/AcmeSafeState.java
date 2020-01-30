3893d44d7125633057a422386d64cc74ce9bc0bc
Alexander Ma <alexanderzma@hotmail.com>
2020-01-29 23:22:26 -0800

    Started HW3.

---
+++ hw3/unsafejmm/AcmeSafeState.java
@@ -0,0 +1,14 @@
+class AcmeSafeState implements State {
+    private long[] value;
+
+    AcmeSafeState(int length) { value = new long[length]; }
+
+    public int size() { return value.length; }
+
+    public long[] current() { return value; }
+
+    public synchronized void swap(int i, int j) {
+	value[i]--;
+	value[j]++;
+    }
+}
