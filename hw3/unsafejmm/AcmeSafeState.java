9e0434140a9ef4af92563ed31c26483bd095b1d0
Alexander Ma <alexanderzma@hotmail.com>
2020-01-30 22:35:15 -0800

    Implemented Acme

--- hw3/unsafejmm/AcmeSafeState.java
+++ hw3/unsafejmm/AcmeSafeState.java
@@ -1,14 +1,23 @@
+import java.util.concurrent.atomic.AtomicLongArray;
+
 class AcmeSafeState implements State {
-    private long[] value;
+    private AtomicLongArray value;
 
-    AcmeSafeState(int length) { value = new long[length]; }
+    AcmeSafeState(int length) { value = new AtomicLongArray(length); }
 
-    public int size() { return value.length; }
+    public int size() { return value.length(); }
 
-    public long[] current() { return value; }
+    public long[] current() {
+	int length = value.length();
+	long[] array = new long[length];
+	for(int i = 0; i < length; i++){
+		array[i] = value.get(i);
+	}
+	return array;
+    }
 
-    public synchronized void swap(int i, int j) {
-	value[i]--;
-	value[j]++;
+    public void swap(int i, int j) {
+	value.getAndDecrement(i);
+	value.getAndIncrement(j);
     }
 }
