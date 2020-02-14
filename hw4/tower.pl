6898f9faa67b867839fa12e72e92176233fb0717
Alexander Ma <alexanderzma@hotmail.com>
2020-02-13 23:11:42 -0800

    Finished normal tower. Also found ambiguous case.

--- hw4/tower.pl
+++ hw4/tower.pl
@@ -1,6 +1,3 @@
-/* base case, no elements */
-tower(N,T,C) :- N=0,T=[],C=counts([],[],[],[]).
-
 /* check row same */
 rows_equal(R1,R2) :- R1==R2.
 /* variables: row1, row2 */
@@ -19,31 +16,56 @@ left_count(Row,Count) :-
 	left_count(Row,Count,0,0).
 /* variables: Row, Count*/
 /* solve left */
-solve_left(0,[],0).
 solve_left(Size,Row,Count) :-
-	length(Row,Size),fd_all_different(Row),row_less_than(Size,Row),left_count(Row,Count).
+	fd_all_different(Row),row_less_than(Size,Row),left_count(Row,Count).
 /* variables: size, row, counts for row */
+/* check count from right */
+right_count([],X,X,_).
+right_count([H|T],Count,Acc,Last) :-
+	H#<#Last,left_count(T,Count,Acc,Last).
+right_count([H|T],Count,Acc,Last) :-
+	H#>#Last,Plus is Acc+1,left_count(T,Count,Plus,H).
+right_count(Row,Count) :-
+	reverse(Row,Reversed),left_count(Reversed,Count,0,0).
+/* variables: Row, Count */
 /* solve right */
+solve_right(Size,Row,Count) :-
+	fd_all_different(Row),row_less_than(Size,Row),right_count(Row,Count).
 /* variables: size, row, counts for row */
 /* solve row */
+solve_row(Size,Row,Counts) :-
+	length(Row,Size),length(Counts,2),fd_element_var(1,Counts,Left),solve_left(Size,Row,Left),fd_element_var(2,Counts,Right),solve_right(Size,Row,Right).
 /* variables: size, row, counts for row */
 /* solve rows */
+solve_rows(_,[],[[],[]],0).
+solve_rows(Size,[H|T],[[HL|TL],[HR|TR]],0) :-
+	solve_row(Size,H,[HL,HR]),solve_rows(Size,T,[TL,TR],0).
+solve_rows(Size,Array,Counts) :-
+	length(Array,Size),solve_rows(Size,Array,Counts,0).
 /* variables: size, array, counts */
 
-/* check column same */
-/* variables: size, column number, array, boolean */
-/* solve up */
-/* variables: size, column number, array, counts */
-/* solve down */
-/* variables: size, column number, array, counts */
-/* solve column */
-/* variables: size, column number, array, counts */
+/* convert rows to columns */
+lists_firsts_rests([], [], []).
+lists_firsts_rests([[F|Os]|Rest], [F|Fs], [Os|Oss]) :-
+	lists_firsts_rests(Rest, Fs, Oss).
+transpose([], _, []).
+transpose([_|Rs], Ms, [Ts|Tss]) :-
+	lists_firsts_rests(Ms, Ts, Ms1),
+	transpose(Rs, Ms1, Tss).
+transpose([], []).
+transpose([F|Fs], Ts) :-
+	transpose(F, [F|Fs], Ts).
+/* variables: array, output */
 /* solve columns */
+solve_columns(Size,Array,Counts) :-
+	length(Array,Size),transpose(Array,Transposed),solve_rows(Size,Transposed,Counts).
 /* variables: size, array, counts */
-
 /* make sure rows and columns solutions match */
+/* base case, no elements */
+tower(N,T,C) :- N=0,T=[],C=counts([],[],[],[]).
+tower(N,T,C) :-
+	C=counts(Top,Bottom,Left,Right),solve_rows(N,T,[Left,Right]),solve_columns(N,T,[Top,Bottom]).
 
-plain_tower(N,T,C) :- N=0,T=[],C=counts([],[],[],[]).
 /* solve left */
 /* variables: size, row, counts for row */
 /* solve right */
@@ -61,8 +83,9 @@ plain_tower(N,T,C) :- N=0,T=[],C=counts([],[],[],[]).
 /* solve columns */
 /* variables: size, array, counts */
 /* make sure rows and columns solutions match */
+plain_tower(N,T,C) :- N=0,T=[],C=counts([],[],[],[]).
 
-ambiguous(N,C,T1,T2) :- tower(N,T1,C),tower(N,T2,C),T1\=T2.
+ambiguous(N,C,T1,T2) :- tower(N,T1,C),tower(N,T2,C),T1\==T2.
 
 tower_test([[2,3,4,5,1],
 [5,4,1,3,2],
